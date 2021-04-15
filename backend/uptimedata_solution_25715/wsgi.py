c3390441bc99bed9d77fd3ec956726d052db5b2d
Crowdbotics <team@crowdbotics.com>
2021-04-15 12:53:56 +0000

    Update models

--- backend/uptimedata_solution_25715/wsgi.py
+++ backend/uptimedata_solution_25715/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uptimedata_solution_25715.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uptimedata_solution_25715.settings")
 
 application = get_wsgi_application()
